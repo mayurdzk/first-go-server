e4413d3c2c208fcefb6cc3ce09a4ea6d37da07d3
mayurdzk <jhonmathews@gmail.com>
2018-06-09 17:18:33 +0530

    Add a form to add a new person

--- html-renderer.go
+++ html-renderer.go
@@ -1,4 +1,5 @@
 package main
+
 // TODO: Separate into its own package
 
 import (
@@ -20,11 +21,36 @@ func peopleHTMLTemplate() *template.Template {
 </body>
 </html>
 `
+	return createTemplate(tmpl)
+}
+
+func newPersonHTMLTemplate() *template.Template {
+	// TODO: Break this out. Use different templates for post and get.
+	var tmpl = `
+	{{if .Success}}
+	<h1>Thanks!</h1>
+{{else}}
+	<h1>New Person</h1>
+	<form method="POST">
+		<label>Name:</label><br />
+		<input type="text" name="name"><br />
+		<label>Age:</label><br />
+		<input type="text" name="age"><br />
+		<input type="submit">
+	</form>
+{{end}}
+	`
+	return createTemplate(tmpl)
+}
+
+func createTemplate(templateString string) *template.Template {
+	// TODO: Why is a name required?
 	t := template.New("main")
-	t, err := t.Parse(tmpl)
+	t, err := t.Parse(templateString)
 	fmt.Println(t)
 	if err != nil {
 		panic(err)
 	}
+
 	return t
 }
--- main.go
+++ main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"log"
 	"net/http"
+	"strconv"
 )
 
 func main() {
@@ -16,10 +17,29 @@ func main() {
 	})
 
 	router.HandleFunc("/add-person", func(w http.ResponseWriter, r *http.Request) {
-		newPerson := NewPerson()
+		t := newPersonHTMLTemplate()
+
+		if r.Method != http.MethodPost {
+			// Serve the user the form
+			t.Execute(w, nil)
+			return
+		}
+
+		ageString := r.FormValue("age")
+		ageNum, err := strconv.ParseInt(ageString, 10, 8)
+		if err != nil {
+			// TODO: Handle this better. Age needs to be a number between 0-150
+			panic(err)
+		}
+		age := int8(ageNum)
+
+		newPerson := Person{
+			Name: r.FormValue("name"),
+			Age:  age,
+		}
 		people = append(people, newPerson)
 
-		http.Redirect(w, r, "people", http.StatusFound)
+		t.Execute(w, struct{ Success bool }{true})
 	})
 
 	log.Fatal(http.ListenAndServe(":8000", router))
