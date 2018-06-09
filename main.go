d72a5a9773665efab4efb272f084f359ce7f1854
mayurdzk <jhonmathews@gmail.com>
2018-06-09 10:56:37 +0530

    Remove gorilla/mux
    I’m trying to stick to an ‘out-of-the-box’ Go experience. A dependency will be included only if I *really* need it.
    
    We can revisit gorilla/mux when the code grows larger, but for now, It’s not needed.

--- main.go
+++ main.go
@@ -3,12 +3,10 @@ package main
 import (
 	"log"
 	"net/http"
-	"github.com/gorilla/mux"
 )
 
 func main() {
-	// TODO: Why do we need MUX here? We can't we use the regular `http.HandleFunc()` method here, instead?
-	router := mux.NewRouter()
-	router.HandleFunc("/people", getAllPeople).Methods("GET")
+	router := http.NewServeMux()
+	router.HandleFunc("/people", getAllPeople)
 	log.Fatal(http.ListenAndServe(":8000", router))
 }
